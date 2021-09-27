37ac1f26ab934a991ff1f7ac9555e378f45aa431
Swasd06 <91150943+Swasd06@users.noreply.github.com>
2021-09-27 09:59:28 +0530

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,24 @@
 #include <bits/stdc++.h>
  
 using namespace std;
- 
-int main(){
-    int n;
-    cin >> n;
-    while(n>1){
-      cout<<n<<" ";
-      if(n%2==0){
-        n= n/2;
-      }
-      else{
-       n=3*n+1;
-      }
-    cout << n;
 
+
+int main() {
+    int numbers[5];
+
+    cout << "Enter 5 numbers: " << endl;
+
+    //  store input from user to array
+    for (int i = 0; i < 5; ++i) {
+        cin >> numbers[i];
     }
+
+    cout << "The numbers are: ";
+
+    //  print array elements
+    for (int n = 0; n < 5; ++n) {
+        cout << numbers[n] << "  ";
+    }
+
+    return 0;
 }
